62f11f13de152edd1183c20e6289222e4aa546f6
Maurício Eduardo <melandim98@gmail.com>
2024-08-17 14:33:44 -0300

    Versão Final

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import lista_livros, adicionar_livro, editar_livro
+from .views import lista_livros, adicionar_livro, editar_livro, excluir_livro, detalhes_livro
 
 urlpatterns = [
     path('livros/', lista_livros, name='lista_livros'),
     path('livros/adicionar/', adicionar_livro, name='adicionar_livro'),
-    path('livros/editar/<int:livro_id>', editar_livro, name='editar_livro'),
+    path('livros/editar/<int:livro_id>/', editar_livro, name='editar_livro'),
+    path('livros/excluir/<int:livro_id>/', excluir_livro, name='excluir_livro'),
+    path('livros/detalhes/<int:livro_id>/', detalhes_livro, name='detalhes_livro')
 ]
--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Livro
 from .forms import LivroForm
@@ -26,3 +27,37 @@ def editar_livro(request, livro_id):
     else:
         form = LivroForm(instance=livro)
     return render(request, 'livraria/editar_livro.html', {'form': form})
+
+def excluir_livro(request, livro_id):
+    livro = get_object_or_404(Livro, pk=livro_id)
+
+    if request.method == 'POST':
+        livro.delete()
+        return redirect('lista')
+    
+    return render(request, 'livraria/excluir_livro.html', {'livro': livro})
+
+def detalhes_livro(request, livro_id):
+    livro = get_object_or_404(Livro, pk=livro_id)
+    return render(request, 'livraria/detalhes_livro.html', {'livro':livro})
+
+def lista_livros(request):
+    query = request.GET.get('q')
+    ano = request.GET.get('ano')
+
+    livros = Livro.objects.all()
+
+    if query:
+        livros = Livro.objects.filter(titulo__icontains=query) | Livro.objects.filter(autor__icontains=query)
+    else:
+        livros = Livro.objects.all()
+
+    if ano:
+        livros = livros.filter(ano_publicacao=ano)
+
+    # Adicionando a paginação
+    paginator = Paginator(livros, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'livraria/lista_livros.html', {'livros': livros})
